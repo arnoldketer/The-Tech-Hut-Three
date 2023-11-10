dc1c99e016b241ef64166415673e57a6656f841b
Arnold <dioufarnold02@gmail.com>
2023-11-10 17:30:51 +0300

    Added main function

--- FirstMaximum.py
+++ FirstMaximum.py
@@ -1,4 +1,14 @@
 def LargestNumber(numbers):
+    """
+    Finds the largest number in a given list.
+
+    Agrs:
+        numbers (list): A list of numbers.
+
+    Returns:
+        int: The largest number in the list.
+    """
+
     biggest_num= numbers[0]
 
     for num in numbers:
@@ -8,6 +18,16 @@ def LargestNumber(numbers):
     return biggest_num
 
 def SecondLargest(numbers):
+    """
+    Finds the second largest number in a given list.
+
+    Args:
+        numbers (list): A list of numbers.
+
+    Returns:
+        int: The second largest number in the list.
+    
+    """
     first_max= max(numbers[0], numbers[1])
     second_max= min(numbers[0], numbers[1])
 
@@ -19,3 +39,29 @@ def SecondLargest(numbers):
             second_max = numbers[i]
 
     return second_max
+
+def main():
+    """
+    Takes user input for a list of numbers, finds the highest and second highest numbers and
+    calculates the difference
+
+    Args:
+        None
+    
+    Returns:
+        None
+    
+    """
+    numbers= [int(x) for x in input("Enter the numbers: ").split()]
+
+    highest= LargestNumber(numbers)
+    second_highest= SecondLargest(numbers)
+
+    if highest is not None and second_highest is not None:
+        difference= highest - second_highest
+        print("The difference between the highest and second highest number is: ", difference)
+    else:
+        print("Invalid input")
+
+if __name__ == "__main__":
+    main()
